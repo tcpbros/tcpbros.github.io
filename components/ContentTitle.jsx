639586472bd0b1f8d665cb4feeeea08a03e9cdea
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-02-28 11:22:24 -0500

    Add a bit of styles

---
+++ components/ContentTitle.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+
+class ContentTitle extends Component {
+  render () {
+    return (
+      <Link className='content-title-link' to={this.props.url}>
+        <div
+          className="content-title"
+          style={{
+            background: `url('${this.props.background}') no-repeat center`,
+            backgroundSize: 'contain',
+          }}
+        >
+          <div className="content-description">
+            <span className="content-name"> {this.props.title} </span>
+          </div>
+      </div>
+    </Link>
+    );
+  }
+}
+
+ContentTitle.propTypes = {
+  url: React.PropTypes.string.isRequired,
+  background: React.PropTypes.string.isRequired,
+  title: React.PropTypes.string.isRequired,
+}
+
+
+export default ContentTitle;
